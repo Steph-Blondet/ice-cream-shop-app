d1ab48df0d9c9445bd25cb71ba7b30e686c8f6da
Stephanie Blondet <stephanieblondet@Stephanies-iMac.local>
2016-12-10 07:50:28 -0500

    back to my first line of thougt - ice cream index route fixed

--- controllers/iceCreamController.js
+++ controllers/iceCreamController.js
@@ -2,17 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 // MODELS
-// var IceCream = require('../models/icecreams.js');
-var iceCreamInfo = require('../icecreams.js');
+var IceCream = require('../models/icecreams.js');
 
 
 //////// ROUTES
 
 // INDEX
 router.get('/', function(req, res){
-    res.render('icecreams/index.ejs', {
-        allIceCreams: iceCreamInfo
-    });
+    IceCream.find({}, function(err, foundIceCreams){
+        if(err) { console.log(err) }
+        // res.send('list of flavors');
+        res.render('icecreams/index.ejs', {
+            allIceCreams: foundIceCreams
+         });
+     });
 });
 
 
@@ -30,18 +33,3 @@ router.get('/icecreams/:id', function(req, res){
 
 // EXPORT
 module.exports = router;
-
-
-
-
-
-// // INDEX
-// router.get('/', function(req, res){
-//     IceCream.find({}, function(err, foundIceCreams){
-//         if(err) { console.log(err) }
-//         // res.send('list of flavors');
-//         res.render('icecreams/index.ejs', {
-//             allIceCreams: foundIceCreams
-//         });
-//     });
-// });
